1143a9773b2ded794cfb07c1526bac23aa171f87
tracer-ysyx <tracer@ysyx.org>
2026-03-31 14:11:07 +0800

    > sim RTL ysyx_22040000 Gao Zhouhao Linux gao-zhouhao-VMware-Virtual-Platform 6.17.0-19-generic #19~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 23:08:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:11:07 up  5:31,  1 user,  load average: 1.10, 0.38, 0.19

--- npc/csrc/cpu/cpu-exec.cpp
+++ npc/csrc/cpu/cpu-exec.cpp
@@ -34,11 +34,12 @@ static void execute(uint64_t  n) {
         //if(top->rootp->top__DOT__ifu__DOT__wait_ready == 1) difftest_skip_ref();
         //if(top->rootp->top__DOT__ifu__DOT__idle == 1) difftest_skip_ref();
         
-        static int wb_done_r = top->rootp->top__DOT__regfile__DOT__wb_done;
-        if(wb_done_r == 1 && top->rootp->top__DOT__regfile__DOT__wb_done == 0)
+        //static int wb_done_r = top->rootp->top__DOT__regfile__DOT__wb_done;
+        //if(wb_done_r == 1 && top->rootp->top__DOT__regfile__DOT__wb_done == 0)
+        if(top->rootp->top__DOT__regfile__DOT__wb_done_flag == 0)
         
         //if(top->rootp->top__DOT__regfile__DOT__wb_done == 1)
-        trace_and_difftest(top->pc, top->rootp->top__DOT__next_pc, top->rootp->top__DOT__inst);
+            trace_and_difftest(top->pc, top->rootp->top__DOT__next_pc, top->rootp->top__DOT__inst);
         if (npc_state.state != NPC_RUNNING) break;
     }
 }
