8b278e7c361164502e7669929730e1dc112fb3ee
tracer-ysyx <tracer@ysyx.org>
2026-03-31 13:48:38 +0800

    > sim RTL ysyx_22040000 Gao Zhouhao Linux gao-zhouhao-VMware-Virtual-Platform 6.17.0-19-generic #19~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 23:08:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:48:38 up  5:09,  1 user,  load average: 0.25, 0.11, 0.15

--- npc/vsrc/RegisterFile.v
+++ npc/vsrc/RegisterFile.v
@@ -17,20 +17,27 @@ module RegisterFile(
     input                       rf_to_ifu_ready,
     output                      rf_to_ifu_valid
 );
+
+    assign rf_to_ifu_valid = lsu_to_rf_valid;
+    
     reg [31:0] rf [31:0];
-assign rf_to_ifu_valid = lsu_to_rf_valid;
+
     integer i;
     initial begin
         for (i = 0; i < 32; i = i + 1) rf[i] = 32'b0;
     end
+    
+    always @(*) begin
+        wb_done = lsu_to_rf_valid && lsu_rf_we && reset;
+    end
 
     always @(posedge clk) begin
-        if(reset == 0) wb_done <= 0;
-        else if(lsu_to_rf_valid)begin
+        //if(reset == 0) wb_done <= 0;
+        if(lsu_to_rf_valid)begin
             if (lsu_rf_we) if(waddr != 5'b0) rf[waddr] <= wdata;
-            wb_done <= 1;
+            //wb_done <= 1;
         end
-        else wb_done <= 0;
+        //else wb_done <= 0;
     end
     
     assign exu_to_rf_ready = 1;
