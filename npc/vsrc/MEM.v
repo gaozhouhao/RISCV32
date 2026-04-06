c49aba9e342271d6567bb370b1300d29c3316cec
tracer-ysyx <tracer@ysyx.org>
2026-04-07 04:22:14 +0800

    > sim RTL ysyx_22040000 Gao Zhouhao Linux gao-zhouhao-VMware-Virtual-Platform 6.17.0-19-generic #19~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 23:08:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:22:14 up  8:15,  1 user,  load average: 0.45, 0.16, 0.12

--- npc/vsrc/MEM.v
+++ npc/vsrc/MEM.v
@@ -83,7 +83,13 @@ always @(posedge clk) begin
     
     if(req_busy2 == 1) begin
         lsu_reqReady <= 1;
-        busy3 <= random_num + 1;
+        if(lsu_wen == 0) begin
+            busy3 <= random_num + 1;
+        end
+        else begin
+            busy3 <= 1;
+            pmem_write(mem_lsu_addr, mem_lsu_wdata, {4'b0, mem_lsu_wmask});
+        end
         mem_lsu_addr <= lsu_addr;
         mem_lsu_wen <= lsu_wen;
         mem_lsu_wdata <= lsu_wdata;
