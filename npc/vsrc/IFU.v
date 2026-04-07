84f763e3a50a3a641d483f085a9b8dc04f856e46
tracer-ysyx <tracer@ysyx.org>
2026-04-08 04:07:58 +0800

    > sim RTL ysyx_22040000 Gao Zhouhao Linux gao-zhouhao-VMware-Virtual-Platform 6.17.0-19-generic #19~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 23:08:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:07:58 up 14:29,  1 user,  load average: 0.38, 0.24, 0.23

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -89,7 +89,8 @@ always @(posedge clk) begin
     else begin
         state <= next_state;
     resp_busy <= 1;
-    if(state == WAIT && ifu_respValid) resp_busy <= random_num + 1;
+    if(state == WAIT && ifu_respValid) resp_busy <= 1;
+    //if(state == WAIT && ifu_respValid) resp_busy <= random_num + 1;
     if(resp_busy > 0) resp_busy <= resp_busy - 1; 
         if((state == IDLE && ifu_to_idu_ready == 1 && wb_done) || start_up == 0) begin
             //ifu_to_idu_valid <= 1'b1;
