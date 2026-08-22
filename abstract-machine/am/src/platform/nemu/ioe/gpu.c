04ef2b82072e626f46ab42540d6ef08197386766
Gao Zhouhao <2996945797@qq.com>
2026-08-22 14:54:34 +0800

    refactor GPU framebuffer access

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,13 +30,11 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
         uint32_t w = ctl->w;
         uint32_t h = ctl->h;
         uint32_t *src = (uint32_t *)ctl->pixels;
-        uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
         for (int i = 0; i < h; i ++){
             for (int j = 0; j < w; j ++){
-                fb[(y+i)*width + j + x] = src[i*w + j];
+                outl(FB_ADDR + ((y+i)*width + j + x) * 4, src[i*w + j]);
             }
         }
-        //for (int i = 0; i < w * h; i ++) fb[i] = src[i];
     }
     if (ctl->sync) {
     outl(SYNC_ADDR, 1);
